71f9f4d0ef18ef3261697dd4773694f9a892fcc1
73307hank <73307hank@gmail.com>
2025-11-28 17:33:07 +0800

    feat: git actions workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: mode === "production" ? `/${env.VITE_PUBLIC_PATH || ""}/` : "/",
+    plugins: [svelte()],
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  };
+});
